1499cf294202176e2418a66eceee76ca5ff2b50a
Enoch Tetteh <124895503+paaqwesilowelltetteh@users.noreply.github.com>
2026-08-18 14:18:37 +0000

    chore: bump nuxt-bearer-auth to version 0.1.5

--- app/components/layout/AppHeader.vue
+++ app/components/layout/AppHeader.vue
@@ -9,7 +9,7 @@ const config = useRuntimeConfig()
 const isCommandPaletteOpen = ref(false)
 const isMobileMenuOpen = ref(false)
 const isScrolled = ref(false)
-const moduleVersion = computed(() => String(config.public.moduleVersion || 'v0.1.4'))
+const moduleVersion = computed(() => String(config.public.moduleVersion || 'v0.1.5'))
 
 function onScroll() {
   isScrolled.value = window.scrollY > 10
